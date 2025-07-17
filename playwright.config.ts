dd33af9442133e81bfbaf2c5990bf55b77d843df
magdalena <magdalena.atanasovska@gmail.com>
2025-07-17 18:22:53 +0200

    update config file junit report

--- playwright.config.ts
+++ playwright.config.ts
@@ -15,7 +15,7 @@ export default defineConfig({
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: [["list"], ["junit", { outputFile: "results/test-results.xml" }]],
+  reporter: [['junit', { outputFile: 'results/test-results.xml' }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     baseURL: "https://fakerestapi.azurewebsites.net",
